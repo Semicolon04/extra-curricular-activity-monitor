a5e8a68e97ef913ee49d9c629c89c8bb7e9dac2e
Sivakar <sivakar12@outlook.com>
2016-12-16 22:21:53 +0530

    Create initial view files

---
+++ resources/views/master.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Extra Curricular Activity Monitor</title>
+
+    </head>
+    <body>
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>
