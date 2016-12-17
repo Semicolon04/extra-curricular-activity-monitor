4571ea7cc2404aa872c434a2fe86530f465e1104
Sivakar <sivakar12@outlook.com>
2016-12-18 00:47:33 +0530

    Complete some methods for staff

--- app/Http/Controllers/StaffController.php
+++ app/Http/Controllers/StaffController.php
@@ -7,79 +7,34 @@ use App\Http\Controllers\Controller;
 
 class StaffController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+        $result = DB::select('SELECT * FROM staff');
+        return view('staff.index', ['staff' => $result])
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
-        //
+        return view('staff.create');
     }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         //
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         //
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
-        //
+        $staff = DB::select("SELECT * FROM staff WHERE id = ?", [$id])[0];
+        return view('staff.edit', ['staff' => $staff]);
     }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         //
     }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        //
+        DB::delete("DELETE FROM staff WHERE id = ?", [$id]);
     }
 }
