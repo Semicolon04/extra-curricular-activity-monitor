9cc8081c008c34c1e1185775edec28b986668370
Sivakar <sivakar12@outlook.com>
2016-12-18 19:47:55 +0530

    Make ActivityController process json input

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -8,27 +8,55 @@ use App\Http\Controllers\Controller;
 class ActivityController extends Controller
 {
     public function storeGenericActivity(Request $request) {
-        // Find the type from the request
-    }
-    public function storeClubActivity(Request $request) {
+        $sql = "INSERT INTO activities"
+            . " (title, description, year, student_id, activity_type)"
+            . " VALUES ?, ?, ?, ?, ?";
+        $values = [$request->input('title'), $request->input('description'),
+            $request->input('year'), $request->input('student_id'),
+            $request->input('activity_type')];
+        $activity_id = DB::select("LAST_INSERT_ID()");
+        $activity_type = $request->input('activity_type');
 
+        if ($request->has('awards')) {
+            for ($award in $request->input('awards')) {
+                $sql = "INSERT INTO awards (activity_id, award_name, "
+                    . "year, organization) VALUES (?, ?, ?, ?)";
+                $values = [$activity_id, $award->award_name,
+                    $award->year, $award->organization];
+                DB::insert($sql, $values);
+            }
+        }
+        if ($activity_type == 'club') {
+            storeClubActivity($request, $activity_id);
+        } else if ($activity_type == 'competition') {
+            storeCompetitionActivity($request, $activity_id);
+        } else if ($activity_type == 'sport') {
+            storeSportsActivity($request, $activity_id);
+        }
     }
-    public function storeCompetitionActivity(Request $request) {
-
+    public function storeClubActivity($request, $activity_id) {
+        $sql = "INSERT INTO club_activities (activity_id, club_name, position)"
+            . " VALUES (?, ?, ?)";
+        $values = [$activity_id, $request->input('club_name'),
+            $request->input('position')];
+        DB::insert($sql, $values);
     }
-    public function storeSportsActivity(Request $request) {
-
+    public function storeCompetitionActivity($request, $activity_id) {
+        $sql = "INSERT INTO competition_activities "
+            . "(activity_id, competition_name, position, competition_organizer)"
+            . " VALUES (?, ?, ?, ?)";
+        $values = [$activity_id, $request->input('competition_name')
+            $request->input('position'), $request->input('competition_organizer')];
+        DB::insert($sql, $values);
     }
-    public function storeClubActivityProject(Request $request, $activity_id) {
-
-    }
-    public function storeSportsEvent(Request $request, $activity_id) {
-
-    }
-    public function storeAwards(Request $request, $activity_id) {
-
+    public function storeSportsActivity($request, $activity_id) {
+        $sql = "INSERT INTO sport_acvities (activity_id, sport_name, position)"
+            . " VALUES (?, ?, ?)";
+        $values = [$activity_id, $request->input('sport_name'),
+            $request->input('position')];
+        DB::insert($sql, $values);
     }
     public function validateActivity(Request $request, $activity_id) {
-        
+
     }
 }
