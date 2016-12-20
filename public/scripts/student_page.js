5927d4c4ac90865cc5ad46ea53517c164cc94639
Sivakar <sivakar12@outlook.com>
2016-12-20 10:12:51 +0530

    Complete form to add club activity

--- public/scripts/student_page.js
+++ public/scripts/student_page.js
@@ -6,15 +6,27 @@ var awardForm = '<div class="container"><div class="form-group col-xs-3">' +
 '</label><input type="text" class="form-control input-sm" name="organization"></div>' +
 '<a class="col-xs-1 remove-award"><br>remove</a></div>';
 
+var projectForm = '<div class="container"><div class="form-group col-xs-3">' +
+'<label for="project_name">Project name:</label><input type="text" class="form-control input-sm"' +
+' name="project_name"></div><div class="form-group col-xs-5">' +
+'<label for="contribution_description">Contribution description:</label>' +
+'<input type="text" class="form-control input-sm" name="contribution_description"></div>' +
+'<a class="col-xs-1 remove-project"><br>remove</a></div>';
 $(document).ready(function() {
     $('button.add-award').click(function() {
         $(this).siblings('.award-fields').append($(awardForm));
     });
 
-    $(document).on('click', 'a.remove-award', function() {
-        $(this).parent().parent().remove();
+    $(document).on('click', '.remove-award', function() {
+        $(this).parent().remove();
     });
 
+    $('button.add-project').click(function() {
+        $(this).siblings('.project-fields').append($(projectForm));
+    });
+    $(document).on('click', '.remove-project', function() {
+        $(this).parent().remove();
+    });
     $('#submit-activity-button').click(function() {
         var activityData = {};
         var $form = $('#new-activity');
@@ -32,6 +44,37 @@ $(document).ready(function() {
             activityData.awards.push(awardDetail);
         });
         activityData.activity_type = 'other';
+        activityData.student_id = STUDENT_ID;    //global var assigned by php
+        alert(JSON.stringify(activityData));
+    });
+
+    $('#submit-club-activity-button').click(function() {
+        var activityData = {};
+        var $form = $('#new-club-activity');
+        activityData.title = $form.find("input[name='title']").val();
+        activityData.description = $form.find("textarea[name='description']").val();
+        activityData.year = $form.find("input[name='year']").val();
+        activityData.club_name = $form.find("input[name='club_name']").val();
+        activityData.post = $form.find("input[name='post']").val();
+        activityData.awards = [];
+        var $awardFields = $('#new-club-activity').find('.award-fields');
+        $awardFields.children().each(function() {
+            awardDetail = {};
+            awardDetail.awardName = $(this).find("input[name='award_name']").val()
+            awardDetail.year = $(this).find("input[name='year']").val();
+            awardDetail.organization = $(this).find("input[name='organization']").val();
+            activityData.awards.push(awardDetail);
+        });
+        activityData.projects = []
+        var $projectFields = $('#new-club-activity').find('.project-fields');
+        $projectFields.children().each(function() {
+            projectDetail = {};
+            projectDetail.project_name = $(this).find("input[name='project_name']").val();
+            projectDetail.contribution_description =
+                $(this).find("input[name='contribution_description']").val();
+            activityData.projects.push(projectDetail);
+        });
+        activityData.activity_type = 'club';
         activityData.student_id = STUDENT_ID;    //global var assigned by php
         alert(JSON.stringify(activityData));
     });
--- resources/views/students/show2.blade.php
+++ resources/views/students/show2.blade.php
@@ -31,9 +31,9 @@
             <button class="btn btn-primary dropdown-toggle"
                 data-toggle="dropdown"><span class="caret"></span></button>
             <ul class="dropdown-menu" role="menu">
-                <li><a data-toggle-"modal" href="#new-club-activity">Club</a></li>
-                <li><a data-toggle-"modal" href="#new-competition">Competition</a></li>
-                <li><a data-toggle-"modal" href="#new-sports">Sports</a></li>
+                <li><a data-toggle="modal" href="#new-club-activity">Club</a></li>
+                <li><a data-toggle="modal" href="#new-competition">Competition</a></li>
+                <li><a data-toggle="modal" href="#new-sports">Sports</a></li>
             </ul>
         </div>
         <h2>Add Activity</h2>
@@ -76,6 +76,52 @@
     </div>
 </div>
 
+<div class="modal fade" id="new-club-activity" role="dialog">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Add new club activity</h4>
+            </div>
+            <div class="modal-body">
+
+                <form class="form-horizontal">
+                    <div class="form-group">
+                        <label for="title" class="control-label col-sm-2">Title</label>
+                        <div class="col-sm-10"><input name="title" class="form-control" type="text"></div>
+                    </div>
+					<div class="form-group">
+                        <label for="club_name" class="control-label col-sm-2">Club Name</label>
+                        <div class="col-sm-10"><input name="club_name" class="form-control" type="text"></div>
+                    </div>
+					<div class="form-group">
+                        <label for="post" class="control-label col-sm-2">Post</label>
+                        <div class="col-sm-10"><input name="post" class="form-control" type="text"></div>
+                    </div>
+                    <div class="form-group">
+                        <label for="description" class="control-label col-sm-2">Description</label>
+                        <div class="col-sm-10"><textarea name="description" class="form-control" type="text"></textarea></div>
+                    </div>
+                    <div class="form-group">
+                        <label for="year" class="control-label col-sm-2">Year</label>
+                        <div class="col-sm-10"><input name="year" class="form-control" type="text"></div>
+                    </div>
+                </form>
+                <button class="btn btn-primary pull-right add-award">Add award</button>
+                <h4>Awards</h4>
+                <br><br><br>
+                <div class="award-fields"></div>
+				<button class="btn btn-primary pull-right add-project">Add project</button>
+				<h4>Projects</h4><br><br><br>
+				<div class="project-fields"></div>
+            </div>
+            <div class="modal-footer">
+                <button class="btn btn-default" data-dismiss="modal">Cancel</button>
+                <button class="btn btn-primary" id="submit-club-activity-button">Add</button>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
 
 @section('scripts')
