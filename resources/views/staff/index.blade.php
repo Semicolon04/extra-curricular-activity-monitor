5ef73e4b6bbb41543911f234593c1369ac7509cc
Sivakar <sivakar12@outlook.com>
2016-12-21 23:16:24 +0530

    Finish views for staff

--- resources/views/staff/index.blade.php
+++ resources/views/staff/index.blade.php
@@ -1,6 +1,33 @@
 @extends('master')
 
 @section('content')
-<h2>List of Staff<h2>
-{{ var_dump($staff)}}
+<div class="container">
+    <div class="col-md-offset-3 col-md-6">
+        <h1>Staff</h1>
+        <a class="btn btn-default pull-right" href="{{route('staff.create')}}">New</a>
+        <br><br>
+
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <td>ID</td>
+                    <td>Name</td>
+                    <td>Job Title</td>
+                    <td></td>
+                </tr>
+            </thead>
+            <tbody>
+            @foreach($staff as $s)
+                <tr>
+                    <td>{{$s->id}}</td>
+                    <td>{{$s->name}}</td>
+                    <td>{{$s->job_title}}</td>
+                    <td><a href="{{route('staff.show', $s->id)}}">
+                        Details</a></td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
 @endsection
--- resources/views/staff/show.blade.php
+++ resources/views/staff/show.blade.php
@@ -1,6 +1,27 @@
 @extends('master')
 @section('content')
-<h2>Staff #{{$staff->id}} Details</h2>
-{{var_dump($staff)}}
-{{var_dump($types)}}
+<div class="container">
+	<div class="col-md-offset-3 col-md-6">
+        <h1>{{$staff->name}}</h1>
+
+		<h3>Staff Details</h3>
+		<a class="btn btn-primary btn-xs pull-right"
+            href="{{route('staff.edit', [$staff->id])}}">Edit Detalils</a>
+
+
+        <table class="table">
+            <tr><td>ID</td><td>{{ $staff->id}}</td></tr>
+            <tr><td>Name</td><td>{{ $staff->name}}</td></tr>
+            <tr><td>Email</td><td>{{ $staff->email}}</td></tr>
+            <tr><td>Job Title</td><td>{{ $staff->job_title}}</td></tr>
+        </table>
+
+        <h3>Categories Managed</h3>
+        <ul>
+            @foreach($types as $c)
+            <li>{{$c}}</li>
+            @endforeach
+        </ul>
+    </div>
+</div>
 @endsection
